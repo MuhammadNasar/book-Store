fa739e40bc92287e5030a3ceecb285f6d14df503
Muhammad Nasar <muhammadnasar008@gmail.com>
2021-05-30 03:05:02 +0500

    security added and entities are added

--- BookStore/src/main/java/com/bookstore/domain/User.java
+++ BookStore/src/main/java/com/bookstore/domain/User.java
@@ -1,74 +1,113 @@
 package com.bookstore.domain;
 
+import java.util.*;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.bookstore.domain.security.Authority;
+import com.bookstore.domain.security.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+//  import sun.awt.www.content.image.gif;
 
 @Entity
-public class User {
+public class User implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name="id", nullable= false, updatable = false)
+	@Column(name = "id", nullable = false, updatable = false)
 	private long id;
 	private String userName;
 	private String password;
 	private String firstName;
 	private String lastName;
-	
-	@Column(name="email", nullable= false, updatable = false)
+
+	@Column(name = "email", nullable = false, updatable = false)
 	private String email;
+	private String phone;
+	private boolean enabled = true;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JsonIgnore
+	private Set<UserRole> userRole = new HashSet<>();
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	public boolean isEnabled() {
-		return enabled;
-	}
+
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
-	private String phone;
-	private boolean enabled = true;
+
+	public Set<UserRole> getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(Set<UserRole> userRole) {
+		this.userRole = userRole;
+	}
+
 	public User(long id, String userName, String password, String firstName, String lastName, String email,
 			String phone, boolean enabled) {
 		super();
@@ -81,10 +120,46 @@ public class User {
 		this.phone = phone;
 		this.enabled = enabled;
 	}
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", userName=" + userName + ", password=" + password + ", firstName=" + firstName
 				+ ", lastName=" + lastName + ", email=" + email + ", phone=" + phone + ", enabled=" + enabled + "]";
 	}
 
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Set<GrantedAuthority> authorities = new  HashSet<>();
+		userRole.forEach(ur -> authorities.add(new 	Authority(ur.getRole().getName())));
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return enabled;
+	}
 }
